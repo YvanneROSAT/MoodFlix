2321b3f46310e2028c9da2bada9cb5f8b8488b35
Yvanne ROSAT <contact@yvannerosat.com>
2025-03-04 02:05:35 +0100

    solve bug two prefix /api

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -15,8 +15,8 @@ const instance = axios.create({
 // Add request interceptor for debugging
 instance.interceptors.request.use(
   config => {
-    // En prod, on s'assure que l'URL commence par /api
-    if (import.meta.env.PROD && !config.url?.startsWith('/api')) {
+    // En prod, on s'assure que l'URL commence par /api seulement si baseURL ne le contient pas déjà
+    if (import.meta.env.PROD && !config.baseURL?.includes('/api') && !config.url?.startsWith('/api')) {
       config.url = `/api${config.url || ''}`;
     }
     
